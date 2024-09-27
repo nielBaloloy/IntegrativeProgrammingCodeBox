d1e4cd40b88583da1aca3b7446b747478fad6ec3
Niel <baloloycrisniel@gmail.com>
2024-09-27 22:50:39 +0800

    Array and Object

---
+++ script/array.js
@@ -0,0 +1,63 @@
+let products = [
+    { name: "John", age: 29, email: "accessories@gmail.com", password: "Accessories", image: "../image/11120556_fi_brands_airbnb_icon.png" },
+];
+
+
+function displaydata(){
+
+    
+let list = document.getElementById('list');
+list.innerHTML = "";
+products.forEach(product => { let card = document.createElement('div');
+        let img = document.createElement('img');
+        let text = document.createElement('div');
+        img.style.width="20px";
+        img.style.height="20px";
+        card.classList.add('card');
+        img.src = product.image; // Set the image source
+     
+        text.innerHTML = `Name: ${product.name}<br>Age: ${product.age}`;
+        
+        card.appendChild(img);
+        card.appendChild(text);
+        list.appendChild(card);
+ 
+})
+}
+
+
+displaydata();
+
+
+let form = document.getElementById("arrayForm");
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    //locate form.image.files[0]
+    let fileImage= form.image.files[0];
+    //create a new FileReader()
+    let reader = new FileReader();
+    //load the reader via reader.onload=function()
+    reader.onload = function(e){
+        let formObjectData = {
+            name:form.name.value,
+            age: form.age.value,
+            email: form.email.value,
+            password: form.password.value,
+            image: e.target.result,
+        }
+      
+        products.push(formObjectData);
+        console.log(products);
+        displaydata();
+    
+        form.reset();
+    }
+    //readAsDataURL() method reads the file as a data URL
+    reader.readAsDataURL(fileImage);
+ });
+
+
+
+
+
---
+++ script/arrayMethods.js
@@ -0,0 +1,44 @@
+
+let user=[{name: 'John', email: 'john@gmail.com', password: '123',age: 99, image: '../image/11120556_fi_brands_airbnb_icon.png'}];
+//display FormData
+function displayFormData(){
+    let resultDisplay = document.getElementById('list');
+    resultDisplay.innerHTML ="";
+    user.forEach(item =>{
+        let text = document.createElement('p')
+        let image = document.createElement('img')
+        image.src=item.image
+        image.style.width="20px"
+        image.style.height="20px"
+        console.log(item)
+        text.innerHTML =`Name: ${item.name}<br> Email: ${item.email}`;
+        resultDisplay.appendChild(image);
+        resultDisplay.appendChild(text);
+})
+}
+displayFormData();
+
+//form
+let form = document.getElementById('arrayForm')
+form.addEventListener('submit',function(event){
+    event.preventDefault();
+    let image = form.image.files[0];
+   
+    let reader = new FileReader();
+    reader.onload = function(e){
+        let ObjectFormValue = {
+            name :form.name.value,
+            age :form.age.value,
+            email : form.email.value,
+            password: form.password.value,
+            image: e.target.result,
+        }
+       
+        user.push(ObjectFormValue);
+        console.log(user);
+        displayFormData();
+    };
+   
+    reader.readAsDataURL(image);
+
+});
